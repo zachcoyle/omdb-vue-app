50b71aa15e9b6739e3b9a55e9ff2ed0262a0aa87
Zach Coyle <zach.coyle@gmail.com>
2023-10-03 16:50:06 -0400

    extract MovieDetail into a component

---
+++ src/components/MovieDetail.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- TODO: Styling -->
+  <div>
+  <h1>{{ this.$store.getters.current.Title}}</h1>
+  <h2>({{ this.$store.getters.current.Year}})</h2>
+  </div>
+  <div>Directed By: {{ this.$store.getters.current.Director}}</div>
+  <div>Featuring: {{ this.$store.getters.current.Actors}}</div>
+  <div v-if="this.$store.getters.current.MetaScore">MetaScore: {{ this.$store.getters.current.MetaScore }}</div>
+  <img v-if="this.$store.getters.current.Poster" v-bind:src="this.$store.getters.current.Poster" />
+</template>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 
 import { search } from '../api'
+import MovieDetail from '../components/MovieDetail.vue';
 
 const query = ref("")
 </script>
@@ -13,16 +14,6 @@ const query = ref("")
       <input v-model="query" type="text" placeholder="Search">
       <button v-on:click="search(query)" class="btn btn-primary">Search</button>
     </div>
-
-    <!-- TODO: turn into movie component -->
-    <!-- TODO: Styling -->
-    <div>
-    <h1>{{ this.$store.getters.current.Title}}</h1>
-    <h2>({{ this.$store.getters.current.Year}})</h2>
-    </div>
-    <div>Directed By: {{ this.$store.getters.current.Director}}</div>
-    <div>Featuring: {{ this.$store.getters.current.Actors}}</div>
-    <div v-if="this.$store.getters.current.MetaScore">MetaScore: {{ this.$store.getters.current.MetaScore }}</div>
-    <img v-if="this.$store.getters.current.Poster" v-bind:src="this.$store.getters.current.Poster" />
+    <MovieDetail />
   </main>
 </template>
